b5b7eb05e14d3b3a41afb837428facb5e90251b7
shuyshuys <ivlyns@protonmail.com>
2024-11-22 03:18:50 +0700

    feat: Update ItemResource with navigation label, plural label, and navigation group

--- app/Filament/Resources/ItemResource.php
+++ app/Filament/Resources/ItemResource.php
@@ -3,10 +3,8 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\ItemResource\Pages;
-use App\Filament\Resources\ItemResource\RelationManagers;
+use App\Filament\Resources\ItemsResource\RelationManagers\StocksRelationManager;
 use App\Models\Item;
-use Filament\Forms;
-use Filament\Forms\Components\Hidden;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
@@ -22,25 +20,30 @@ class ItemResource extends Resource
 {
     protected static ?string $model = Item::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationLabel = 'Barang';
+
+    protected static ?string $pluralLabel = 'Barang';
+
+    protected static ?string $navigationIcon = 'heroicon-o-clipboard-document-list';
+
+    protected static ?string $navigationGroup = 'Manajemen Inventaris';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
                 TextInput::make('code')
+                    ->label('KODE')
                     ->required()
                     ->maxLength(255),
                 TextInput::make('name')
+                    ->label('NAMA')
                     ->required()
                     ->maxLength(255),
                 Select::make('unit_id')
+                    ->label('SATUAN')
                     ->relationship('unit', 'name')
                     ->required(),
-                // Hidden::make('created_by')
-                //     ->default(Auth::user()->id),
-                // Hidden::make('updated_by')
-                //     ->default(Auth::user()->id),
             ]);
     }
 
@@ -51,25 +54,38 @@ class ItemResource extends Resource
                 TextColumn::make('id')
                     ->sortable()
                     ->searchable(),
-                TextColumn::make('code')->label('Kode'),
-                TextColumn::make('name')->label('Nama'),
-                TextColumn::make('unit.name')->label('Satuan'),
+                TextColumn::make('code')
+                    ->label('Kode')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('name')
+                    ->label('Nama')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('unit.name')
+                    ->label('Satuan')
+                    ->sortable()
+                    ->searchable(),
                 TextColumn::make('creator.name')
                     ->label('Dibuat Oleh')
                     ->sortable()
-                    ->searchable(),
+                    ->searchable()
+                    ->badge()
+                    ->colors([
+                        'success' => fn($record) => $record->creator->role == 'manajemen_keuangan',
+                        'info' => fn($record) => $record->creator->role == 'maanajemen_gudang',
+                        'danger' => fn($record) => $record->creator->role == 'superadmin',
+                    ]),
                 TextColumn::make('updater.name')
                     ->label('Diperbarui Oleh')
                     ->sortable()
-                    ->searchable(),
-                TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                    ->searchable()
+                    ->badge()
+                    ->colors([
+                        'success' => fn($record) => $record->creator->role == 'manajemen_keuangan',
+                        'info' => fn($record) => $record->creator->role == 'maanajemen_gudang',
+                        'danger' => fn($record) => $record->creator->role == 'superadmin',
+                    ]),
             ])
             ->filters([
                 //
@@ -87,7 +103,7 @@ class ItemResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            StocksRelationManager::class,
         ];
     }
 
