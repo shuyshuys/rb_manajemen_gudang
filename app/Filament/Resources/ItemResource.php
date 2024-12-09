a07fcb40b3c283654c5cf5c0a06036aaa56d13c6
shuyshuys <ivlyns@protonmail.com>
2024-12-10 00:13:21 +0700

    refactor: Update ItemResource table columns and add filter

--- app/Filament/Resources/ItemResource.php
+++ app/Filament/Resources/ItemResource.php
@@ -2,19 +2,20 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\ItemResource\Pages;
-use App\Filament\Resources\ItemsResource\RelationManagers\StocksRelationManager;
 use App\Models\Item;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
-use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Resources\Resource;
+use Filament\Tables\Filters\Filter;
 use Illuminate\Support\Facades\Auth;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\ItemResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\ItemsResource\RelationManagers\StocksRelationManager;
 
 class ItemResource extends Resource
 {
