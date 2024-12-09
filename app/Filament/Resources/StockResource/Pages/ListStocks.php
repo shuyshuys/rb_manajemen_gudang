b794daf042fa69ccdecfd975c3ea0048244fcffe
shuyshuys <ivlyns@protonmail.com>
2024-12-10 00:11:16 +0700

    feat: Add export excel functionality to ListStocks page

--- app/Filament/Resources/StockResource/Pages/ListStocks.php
+++ app/Filament/Resources/StockResource/Pages/ListStocks.php
@@ -5,6 +5,10 @@ namespace App\Filament\Resources\StockResource\Pages;
 use App\Filament\Resources\StockResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use pxlrbt\FilamentExcel\Columns\Column;
+use Carbon\Carbon;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
+use pxlrbt\FilamentExcel\Actions\Pages\ExportAction;
 
 class ListStocks extends ListRecords
 {
@@ -14,6 +18,12 @@ class ListStocks extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+            ExportAction::make()
+                ->exports([
+                    ExcelExport::make()
+                        ->fromTable()
+                        ->withFilename(fn($resource) =>  'Inventaris-Semua-Barang-' . date('Y-m-d'))
+                ]),
         ];
     }
 }
